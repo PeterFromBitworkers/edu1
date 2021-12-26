a5c8e4853e109bd53e2dd3c079cc305243e766b2
peter <p.biermann@bitworkers.eu>
2021-12-26 18:00:00 +0100

    add prompts

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
-console.log("hello")
-let a = 2
-console.log (a+1)
+let rot13 = (message) => {
+    
+    const input = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
+    const output = 'NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm';
+    let encoded = '';
+    
+    for (let i=0; i < message.length; i++) {
+        const index = input.indexOf(message[i]);
+        encoded += output[index];
+    }
 
+    return encoded;
+}
+
+let input = "hello"
+let output = rot13(input)
+console.log(output)
